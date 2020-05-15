3d227621e5dd3759bd0fb0e9feff1b0021ace30a
Nargiza6 <65377205+Nargiza6@users.noreply.github.com>
2020-05-14 20:49:36 -0500

    My home work 05.14.2020

---
+++ MyName.java
@@ -0,0 +1,9 @@
+public class MyName {
+	public static void main(String[] args) {
+		String fistName= "John";
+		String lastName = "Doe";
+		System.out.println("My first Name is " + fistName +  "  and last Name is "
+		 +  lastName);
+	}
+
+}
